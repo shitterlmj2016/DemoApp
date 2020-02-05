10c014891bcda32272e30c0f0f8dd813deb359a7
shitterlmj2016 <915930987@qq.com>
2020-02-04 23:21:11 -0800

    inital commit

---
+++ src/com/xinchenh/AddServlet.java
@@ -0,0 +1,20 @@
+package com.xinchenh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.valueOf(req.getParameter("num1"));
+		int j = Integer.valueOf(req.getParameter("num2"));
+		
+		int k = i + j;
+		//System.out.println(k);
+		PrintWriter out = res.getWriter(); //print data on a response object
+		out.println("The result is "+k);
+	}
+}
